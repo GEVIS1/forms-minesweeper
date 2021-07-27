948ca92212d91d320a0f9f9d70d73e0d1a167f9e
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2021-07-27 12:45:05 +1200

    Init: Project initializedClass: Add MineField.cs class

---
+++ forms-minesweeper/MineField.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forms_minesweeper
+{
+    public class MineField
+    {
+        public int FieldSize;
+        public int Difficulty;
+        public Button[] GameField;
+
+        public MineField(int FieldSize, int Difficulty)
+        {
+            this.FieldSize = FieldSize;
+            this.Difficulty = Difficulty;
+        }
+    }
+}
