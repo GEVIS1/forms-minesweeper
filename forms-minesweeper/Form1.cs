63c25469d0db15c17e92138d7c5ab234b33ca33d
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2021-07-27 12:45:05 +1200

    Init: Project initialized Class: Add MineField.cs class

---
+++ forms-minesweeper/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forms_minesweeper
+{
+    public partial class Form1 : Form
+    {
+
+        MineField mf;
+        public Form1()
+        {
+            InitializeComponent();
+            mf = new MineField(5,5);
+        }
+    }
+}
